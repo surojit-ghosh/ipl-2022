2a625530985b76ffcaf98d3d71d1c56c01047667
surojit-cyberpeak <surojit@cyberpeakitsolutions.com>
2026-09-03 15:20:20 +0530

    fix: backend url in configs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const backendUrl = process.env.BACKEND_URL || "http://localhost:3001";
+const backendUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 const nextConfig: NextConfig = {
   turbopack: {},
